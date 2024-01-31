371bf67c069ae7f9e668f54c4105280b76da0b19
Robert Lanzafame <R.C.Lanzafame@tudelft.nl>
2024-01-31 06:27:12 +0100

    test mod

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import http.server, ssl
 
-server_address = ('localhost', 4443)
+server_address = ('localhost', 8889)
 httpd = http.server.HTTPServer(server_address, http.server.SimpleHTTPRequestHandler)
 httpd.socket = ssl.wrap_socket(httpd.socket,
                                server_side=True,
